63b1384ff270bff683eb9b2b64dd970f4bb6ef19
Adam Moore <nerdfunk@gmail.com>
2017-12-05 18:37:14 +0900

    add int-array version for day 5

--- src/advent/2017/day5.clj
+++ src/advent/2017/day5.clj
@@ -6,7 +6,7 @@
   (->> (io/resource "day5.txt")
        slurp
        s/split-lines
-       (mapv read-string)))
+       (mapv #(Integer/parseInt %))))
 
 (defn solve [input]
   (loop [i input idx 0 c 0]
@@ -31,14 +31,28 @@
   (loop [i (transient input) idx 0 c 0]
     (if (or (> 0 idx)
             (<= (count input) idx)) c
-        (let [v (get i idx)]
+        (let [^long v (get i idx)]
           (recur (if (<= 3 v)
-                   (assoc! i idx (dec (get i idx)))
-                   (assoc! i idx (inc (get i idx))))
+                   (assoc! i idx (dec v))
+                   (assoc! i idx (inc v)))
                  (+ idx v)
                  (inc c))))))
+
+(defn solve2-int-array [input]
+  (let [len (count input)
+        i (int-array input)]
+    (loop [idx 0 c 0]
+      (if (or (> 0 idx) (<= len idx)) c
+          (let [v (aget i idx)]
+            (if (<= (int 3) v)
+              (aset-int i idx (dec v))
+              (aset-int i idx (inc v)))
+            (recur (+ idx v)
+                   (inc c)))))))
 
 (comment (solve input)
          (solve2-transient [0 3 0 1 -3])
          (solve2 input)
-         (solve2-transient input))
+         (solve2-transient input)
+         (solve2-int-array int-arr-input)
+         )
