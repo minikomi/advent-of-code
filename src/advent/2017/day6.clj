32b1a88a7d78aeef5c68447d80477c5dd22c3425
Adam Moore <nerdfunk@gmail.com>
2017-12-06 14:45:00 +0900

    refine day6

--- src/advent/2017/day6.clj
+++ src/advent/2017/day6.clj
@@ -7,18 +7,19 @@
   [0 2 7 0])
 
 (defn get-indexed-max [memory-state]
-  (loop [vs memory-state
-         idx 0
-         current-max [nil -1]
-         ]
-    (if (empty? vs) current-max
-        (recur (rest vs)
-               (inc idx)
-               (if (< (second current-max) (first vs))
-                 [idx (first vs)]
-                 current-max)))))
+  (loop [idx 0
+         current-max [nil -1]] ;; no values below 0
+    (if-let [v (get memory-state idx)]
+      (recur (inc idx)
+             (if (< (second current-max) v)
+               [idx v]
+               current-max))
+      current-max)))
 
-(comment (get-indexed-max input))
+(comment
+  (get-indexed-max test-input) ;; [2 7]
+  (get-indexed-max input)      ;; [11 15]
+  )
 
 (defn step [memory-state]
   (let [[max-idx max-val] (get-indexed-max memory-state)]
@@ -29,23 +30,16 @@
             (range 1 (inc max-val)))))
 
 (defn solve [memory-state]
-  (loop [current-memory memory-state seen #{memory-state} cycles 0]
-    (let [new-state (step current-memory)]
-      (if (seen new-state) cycles
-          (recur new-state
-                 (conj seen new-state)
-                 (inc cycles))))))
-
-
-(defn solve2 [memory-state]
   (loop [current-memory memory-state seen {memory-state 0} cycles 0]
     (let [new-state (step current-memory)
           new-cycle-count (inc cycles)]
-      (if (seen new-state) (- new-cycle-count (seen new-state))
-          (recur new-state
-                 (assoc seen new-state new-cycle-count)
-                 new-cycle-count)))))
+      (if (seen new-state)
+        {:cycle-count new-cycle-count
+         :loop-distance (- new-cycle-count (seen new-state))}
+        (recur new-state
+               (assoc seen new-state new-cycle-count)
+               new-cycle-count)))))
 
-(comment (solve2 test-input))
+(comment (solve test-input))
 
-(comment (println (solve2 input)))
+(comment (println (solve input)))
