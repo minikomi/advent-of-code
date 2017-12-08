e005fc225b9ed3b1854ee46ffd975127a2f02957
Adam Moore <nerdfunk@gmail.com>
2017-12-08 12:20:55 +0900

    util macro

---
+++ src/advent/util.clj
@@ -0,0 +1,18 @@
+(ns advent.util)
+
+(defmacro let-stop [bindings & body]
+  (if (some #{'*stop*} bindings)
+    (let [stopped-bindings
+          (->> bindings
+               (take-while #(not= '*stop* %))
+               vec)
+          caught-symbols
+          (->> stopped-bindings
+               (partition 2)
+               (map #(vector (keyword (first %)) (first %)))
+               (into {}))]
+      `(let ~stopped-bindings
+         (throw (ex-info (str "Let was stopped at stage "
+                              (/ (count ~stopped-bindings) 2))
+                         {:current-symbols ~caught-symbols}))))
+    `(let* ~(destructure bindings) ~@body)))
