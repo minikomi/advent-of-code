c428ca38ff5569d59c950f48ac2a368dc55fc1b1
Adam Moore <nerdfunk@gmail.com>
2017-12-11 14:31:00 +0900

    day 11

---
+++ src/advent/2017/day11.clj
@@ -0,0 +1,39 @@
+(ns advent.2017.day11
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [advent.util :as util]))
+
+(def input-raw
+  (s/trim (slurp (io/resource "day11.txt"))))
+
+(def get-dir
+  {"n" [0 1 -1]
+   "s" [0 -1 1]
+   "ne" [1 0 -1]
+   "nw" [-1 1 0]
+   "se" [1 -1 0]
+   "sw" [-1 0 1]
+   })
+
+(defn step [state action]
+  (let [[x y z] (get-dir action)]
+    (-> state
+        (update 0 + x)
+        (update 1 + y)
+        (update 2 + z))))
+
+(defn distance [[x y z]]
+  (/ (+ (Math/abs x) (Math/abs y) (Math/abs z)) 2))
+
+
+(comment (distance (reduce step [0 0 0] ["ne" "ne" "ne"]))
+
+         (distance (reduce step [0 0 0] ["ne","ne","sw","sw"]))
+
+         (distance (reduce step [0 0 0] ["ne","ne","s","s"]))
+
+         (distance (reduce step [0 0 0] ["se","sw","se","sw","sw"]))
+
+         (distance (reduce step [0 0 0] (s/split input-raw #",")))
+
+         (apply max (map distance (reductions step [0 0 0] (s/split input-raw #",")))))
