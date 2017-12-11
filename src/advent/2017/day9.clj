709fea5e29b9350069f6f038f6b14eb455ef7421
Adam Moore <nerdfunk@gmail.com>
2017-12-11 11:24:47 +0900

    day 9

---
+++ src/advent/2017/day9.clj
@@ -0,0 +1,36 @@
+(ns advent.2017.day9
+  (:require [clojure.java.io :as io]))
+
+(def input (slurp (io/resource "day9.txt")))
+
+(defn solve1 [input]
+  (loop [s (vec (reverse input))
+         lvl 0
+         garbage false
+         gc 0
+         total 0]
+    (if-let [c (peek s)]
+      (if garbage
+        (case c
+          \> (recur (pop s) lvl false gc total)
+          \! (recur (pop (pop s)) lvl garbage gc total)
+          (recur (pop s) lvl true (inc gc) total))
+        (case c
+          \{ (recur (pop s) (inc lvl) garbage gc total)
+          \} (recur (pop s) (dec lvl) garbage gc (+ total lvl))
+          \< (recur (pop s) lvl true gc total)
+          \, (recur (pop s) lvl garbage gc total)
+          \newline (recur (pop s) lvl garbage gc total)
+          \! (recur (pop (pop s)) lvl garbage gc total)
+          (throw (ex-info "weird state" {:s s :lvl lvl :total total}))))
+      [garbage gc lvl total])))
+
+(comment
+  (solve1 "{}")
+  (solve1 "{{{}}}")
+  (solve1 "{{<!!>},{<!!>},{<!!>},{<!!>}}")
+  (solve1 "{{<a!>},{<a!>},{<a!>},{<ab>}}")
+
+  (solve1 "<{o\"i!a,<{i<a>") ;; gc 10
+
+  (solve1 input))
