540dc5b0a00c504e9f7d8688f0a13dc51c2299d6
Adam Moore <nerdfunk@gmail.com>
2017-12-13 16:20:42 +0900

    day13 first attempt -- slow!

---
+++ src/advent/2017/day13.clj
@@ -0,0 +1,41 @@
+(ns advent.2017.day13
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [advent.util :as util]
+            [clojure.set :as set]))
+
+(def input-raw
+  (s/trim (slurp (io/resource "day13.txt"))))
+
+(def get-states
+  (memoize
+   (fn [scan-range]
+     (concat (range (dec scan-range))
+             (range (dec scan-range) 0 -1)))))
+
+(def test-input
+  "0: 3
+1: 2
+4: 4
+6: 4")
+
+(defn parse-input [input]
+  (into {}
+        (mapv #(vec (map read-string (s/split % #": ")))
+              (s/split-lines input))))
+
+(parse-input test-input)
+
+(defn solve1 [parsed delay]
+  (->> (let [max-slot (apply max (mapv first parsed))]
+         (for [i (range (inc max-slot))
+               :let [scan-range (get parsed i)]
+               :when (and scan-range
+                          (zero? (nth (get-states scan-range)
+                                      (mod (+ i delay)
+                                           (- (* scan-range 2) 2)))))]
+           (* i scan-range)))))
+
+(defn solve2 [input]
+  (let [parsed (parse-input input)]
+    (count (take-while seq (map #(solve1 parsed %) (range))))))
